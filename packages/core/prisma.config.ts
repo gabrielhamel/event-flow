e6f5f0b2fcd4c850857f932c98cea37d943f5cc7
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-03-09 11:15:47 +0100

    setup prisma

---
+++ packages/core/prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "./src/infra/prisma/schema.prisma",
+  migrations: {
+    path: "./src/infra/prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
