f566a1a51af2262a760744f01fca7dfa294e0e3f
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-03-23 14:46:47 +0100

    add vitest configs for all packages and apps

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,11 +1,10 @@
-import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths()],
   test: {
-    include: ["./src/**/*.test.{ts,tsx}"],
+    include: ["./src/**/*.test.ts"],
     passWithNoTests: true,
   },
 });
