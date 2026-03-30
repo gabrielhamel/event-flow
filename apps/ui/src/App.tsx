4b99bb600fee7f0f3b8626f307e96df7d89fa9b1
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-03-30 14:32:23 +0200

    add the logout feature

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,13 +1,25 @@
-import { Avatar, Box, Button, createTheme, CssBaseline, Skeleton, ThemeProvider } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  createTheme,
+  CssBaseline,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Skeleton,
+  ThemeProvider,
+} from "@mui/material";
 import { EventStormingControlBar } from "./event-storming/components/EventStormingControlBar";
 import { WhiteboardComponent } from "./whiteboard/components/WhiteboardComponent";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Logout } from "@mui/icons-material";
 import { getInfraConfig } from "@repo/core/infra/config";
 import { createAuthClient } from "better-auth/react";
-import { useState } from "react";
+import { type MouseEvent, useState } from "react";
 
 const isInDevelopment = import.meta.env.MODE === "development";
 const { apiBaseUrl, uiBaseUrl } = getInfraConfig(isInDevelopment);
@@ -26,6 +38,7 @@ const App = () => {
   const { data: session, isPending, isRefetching } = auth.useSession();
 
   const [isSignIn, setIsSignIn] = useState(false);
+  const [anchorAvatarMenu, setAnchorAvatarMenu] = useState<null | HTMLElement>(null);
 
   const avatarData = session?.user.name.split(" ").map(word => word[0]).join("") ?? "";
   const isSessionLoading = isPending || isRefetching;
@@ -39,6 +52,22 @@ const App = () => {
     }).catch((error: unknown) => {
       console.error("Error signing in with GitHub:", error);
       setIsSignIn(false);
+    });
+  };
+
+  const handleAvatarClick = (event: MouseEvent<HTMLDivElement>) => {
+    setAnchorAvatarMenu(event.currentTarget);
+  };
+
+  const handleAvatarMenuClose = () => {
+    setAnchorAvatarMenu(null);
+  };
+
+  const handleSignOut = () => {
+    setAnchorAvatarMenu(null);
+
+    auth.signOut().catch((error: unknown) => {
+      console.error("Error signing out:", error);
     });
   };
 
@@ -58,15 +87,37 @@ const App = () => {
         >
           {isSessionLoading && <Skeleton variant="circular" width={40} height={40} />}
           {(!isSessionLoading && !session) && (
-            <Button
-              variant="outlined"
-              onClick={handleSignInWithGithub}
-              loading={isSignIn}
-            >
-              SignIn
-            </Button>
+            <>
+              <Button
+                variant="outlined"
+                onClick={handleSignInWithGithub}
+                loading={isSignIn}
+              >
+                Sign in
+              </Button>
+            </>
           )}
-          {(!isSessionLoading && session) && <Avatar>{avatarData}</Avatar>}
+          {(!isSessionLoading && session) && (
+            <>
+              <Avatar
+                component="div"
+                sx={{
+                  boxShadow: 1,
+                }}
+                onClick={handleAvatarClick}
+              >
+                {avatarData}
+              </Avatar>
+              <Menu open={Boolean(anchorAvatarMenu)} onClose={handleAvatarMenuClose} anchorEl={anchorAvatarMenu}>
+                <MenuItem onClick={handleSignOut}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Sign out
+                </MenuItem>
+              </Menu>
+            </>
+          )}
         </Box>
 
         <Box
