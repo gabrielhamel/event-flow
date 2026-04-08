598621a3b4f38a7d2faae4f49b82cb7b1885a014
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-04-08 23:17:58 +0200

    fix lint and format issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     "*": "vp check --fix",
   },
   fmt: {
+    ignorePatterns: ["apps/ui/src/routes.ts"],
     sortImports: {
       newlinesBetween: false,
       groups: [
