2fb521e39eb3f401bca034dd846882d045c0fde8
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-04-10 16:40:59 +0200

    redirect auth to / if there is no path specified

--- apps/ui/src/routes/auth.tsx
+++ apps/ui/src/routes/auth.tsx
@@ -1,6 +1,6 @@
 import { GitHub, Google } from "@mui/icons-material";
 import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 import z from "zod";
 import { CenteredLayout } from "../components/CenteredLayout";
 import { useSession } from "../session/hooks/useSession";
@@ -15,12 +15,15 @@ export const Route = createFileRoute("/auth")({
 function AuthPage() {
   const { signIn, isSignInLoading } = useSession();
   const { redirectPath } = Route.useSearch();
-  const redirectPathWithDefault = redirectPath ?? "/";
+  const router = useRouter();
+
+  if (!redirectPath) {
+    return router.navigate({ to: "/" });
+  }
 
   return (
     <CenteredLayout>
       <Stack borderRadius={2} p={2} spacing={2}>
-        <Typography>You should be authenticated to see this page</Typography>
         {isSignInLoading ? (
           <Box
             sx={{
@@ -32,10 +35,11 @@ function AuthPage() {
           </Box>
         ) : (
           <>
+            <Typography>You should be authenticated to see this page</Typography>
             <Button
               variant="contained"
               startIcon={<GitHub />}
-              onClick={() => signIn("github", redirectPathWithDefault)}
+              onClick={() => signIn("github", redirectPath)}
               loading={isSignInLoading}
             >
               Github
@@ -43,7 +47,7 @@ function AuthPage() {
             <Button
               variant="contained"
               startIcon={<Google />}
-              onClick={() => signIn("google", redirectPathWithDefault)}
+              onClick={() => signIn("google", redirectPath)}
               loading={isSignInLoading}
             >
               Google
