eb9fdda2bd50b8836a2bce725237bc42a0c9ac7f
Gabriel Hamel <gabriel.hamel@fabriq.tech>
2026-04-15 14:04:47 +0200

    instanciate @hocuspocus/extension-database

--- apps/api/src/websocket.ts
+++ apps/api/src/websocket.ts
@@ -1,7 +1,22 @@
+import { Database } from "@hocuspocus/extension-database";
 import { Hocuspocus } from "@hocuspocus/server";
 
 export const websocketHandler = new Hocuspocus({
   onAwarenessUpdate() {
     return Promise.resolve();
   },
+  extensions: [
+    new Database({
+      fetch: (data) => {
+        console.log("fetch", data);
+
+        return Promise.resolve(null);
+      },
+      store: (data) => {
+        console.log("store", data);
+
+        return Promise.resolve();
+      },
+    }),
+  ],
 });
