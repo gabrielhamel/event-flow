f0ca91654b21f040ce865c66a611a753c6c66392
Gabriel Hamel <gabriel.hamel.pro@gmail.com>
2026-04-20 23:37:51 +0200

    Retrieve the document from the event storming directly

--- apps/api/src/websocket.ts
+++ apps/api/src/websocket.ts
@@ -1,8 +1,29 @@
+import type { Id } from "@repo/core/Id";
 import { Database } from "@hocuspocus/extension-database";
 import { Hocuspocus } from "@hocuspocus/server";
+import { PrismaEventStormingRepository } from "@repo/core/event-storming/infra/PrismaEventStormingRepository";
+import { CreateEventStormingUseCase } from "@repo/core/event-storming/useCase/CreateEventStorming";
+import { GetEventStormingUseCase } from "@repo/core/event-storming/useCase/GetEventStorming";
 import { makePrismaClient } from "@repo/core/infra/prisma/index";
+import { PrismaUserRepository } from "@repo/core/user/infra/PrismaUserRepository";
+import { GetUserUseCase } from "@repo/core/user/useCase/GetUserUseCase";
 
 const db = makePrismaClient();
+
+const userRepository = new PrismaUserRepository(db);
+const eventStormingRepository = new PrismaEventStormingRepository(db);
+
+const container = {
+  useCase: {
+    user: {
+      get: new GetUserUseCase(userRepository),
+    },
+    eventStorming: {
+      get: new GetEventStormingUseCase(eventStormingRepository),
+      create: new CreateEventStormingUseCase(userRepository, eventStormingRepository),
+    },
+  },
+};
 
 export const websocketHandler = new Hocuspocus({
   onAwarenessUpdate() {
@@ -11,11 +32,13 @@ export const websocketHandler = new Hocuspocus({
   extensions: [
     new Database({
       async fetch({ documentName }) {
-        const document = await db.document.findUnique({
-          where: { id: documentName },
-        });
+        const eventStorming = await container.useCase.eventStorming.get.execute(documentName as Id);
 
-        return document?.data ?? null;
+        if (eventStorming.isErr()) {
+          return null;
+        }
+
+        return eventStorming.value.data;
       },
       async store({ documentName, state }) {
         const data = new Uint8Array(state);
